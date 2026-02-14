033ea18c065dbc3cfa71bf21f0cc1b3d440caac2
L-Bayrem <160652712+Lamouchi-Bayrem@users.noreply.github.com>
2026-02-14 22:31:31 +0100

    Update roi.py

--- core/roi.py
+++ core/roi.py
@@ -1,5 +1,11 @@
-def crop(image, x1, y1, x2, y2):
+def crop_roi(image, coords):
+    if coords is None:
+        return image
+
+    x1, y1, x2, y2 = coords
     h, w = image.shape[:2]
+
     x1, y1 = max(0, x1), max(0, y1)
     x2, y2 = min(w, x2), min(h, y2)
+
     return image[y1:y2, x1:x2]
