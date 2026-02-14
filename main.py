69044ad22600499e64fd4d4ebb1450e51bca0210
L-Bayrem <160652712+Lamouchi-Bayrem@users.noreply.github.com>
2026-02-14 22:48:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+# main.py
+
 import sys
 from PyQt6.QtWidgets import QApplication
 from gui.qt_app import CVStudio
 
 def main():
     app = QApplication(sys.argv)
+    app.setApplicationName("CV Studio - Advanced CV Preprocessing & Augmentation")
+
     window = CVStudio()
     window.show()
+
     sys.exit(app.exec())
 
 if __name__ == "__main__":
